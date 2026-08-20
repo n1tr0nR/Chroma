ccbd2dcca974e5e7bf63d10cbbc40f52a434c468
Nitron <n1tr0nrbn@gmail.com>
2026-08-20 13:39:36 +0100

    holy bug fix :sob:

--- src/main/java/dev/rbn/chroma/submods/flare/mixin/CameraMixin.java
+++ src/main/java/dev/rbn/chroma/submods/flare/mixin/CameraMixin.java
@@ -3,6 +3,7 @@ package dev.rbn.chroma.submods.flare.mixin;
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import dev.rbn.chroma.client.Chroma;
+import dev.rbn.chroma.config.Config;
 import dev.rbn.chroma.config.GlobalConfigHandler;
 import dev.rbn.chroma.submods.flare.screenshake.Screenshake;
 import net.minecraft.client.Camera;
@@ -11,20 +12,19 @@ import net.minecraft.resources.Identifier;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 
-import java.util.Objects;
-
 @Mixin(Camera.class)
 public abstract class CameraMixin {
     @WrapOperation(method = "setup", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/Camera;setRotation(FF)V"))
     private void chroma$applyScreenshake(Camera instance, float yaw, float pitch, Operation<Void> original){
         Minecraft minecraft = Minecraft.getInstance();
+        Config config = GlobalConfigHandler.instance.getConfigForId(Chroma.MOD_ID);
+        if (config == null) {
+            original.call(instance, yaw, pitch);
+            return;
+        }
 
-        boolean enabled = Objects.requireNonNull(GlobalConfigHandler.instance.getConfigForId(Chroma.MOD_ID)).get(
-                Identifier.fromNamespaceAndPath(Chroma.MOD_ID, "flare_enabled")
-        );
-        float mult = Objects.requireNonNull(GlobalConfigHandler.instance.getConfigForId(Chroma.MOD_ID)).get(
-                Identifier.fromNamespaceAndPath(Chroma.MOD_ID, "force_multiplier")
-        );
+        boolean enabled = config.get(Identifier.fromNamespaceAndPath(Chroma.MOD_ID, "flare_enabled"));
+        float mult = config.get(Identifier.fromNamespaceAndPath(Chroma.MOD_ID, "force_multiplier"));
 
         if (minecraft.level != null && enabled){
             float intensity = Screenshake.getInstance().getCombinedStrength(minecraft.getDeltaTracker().getGameTimeDeltaPartialTick(false)) * mult;
